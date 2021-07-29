0895aebfa1e1acb4532eb82c58b194410f6c8e27
jynadal <nadaljeanyves@hotmail.fr>
2021-07-29 20:57:31 +0200

    first commit after cloning

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from "../components/SEO"
 export default function Home() {
   return (
     <Layout>
-      <SEO title="Home " />
+      <SEO title="Hommmmeeee " />
       <main className="page">
         <header className="hero">
           <StaticImage
